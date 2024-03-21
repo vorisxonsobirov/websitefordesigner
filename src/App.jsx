f14751b8ce2a7f8b8f72ce76371f5c6d8daa9fda
Murodillaxon <murka.surka.a4@gmail.com>
2024-03-21 12:14:32 +0500

    HomePage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import simpleParallax from "simple-parallax-js";
+import "./style.css";
+import Home from "./components/Home/Home";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Logo from "./3dLogo";
+import "./style.css";
+import { color, motion } from "framer-motion";
+import { TypeAnimation } from "react-type-animation";
+import { fadeIn } from "../../variants";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="Navbar">
+        <div className="logo"></div>
+        <ul>
+          <li>Eng</li>
+          <li>Портфолио</li>
+          <li>Пакеты</li>
+          <button>Оставить заявку</button>
+        </ul>
+      </div>
+      <div className="home-content">
+        <div className="home-content-text">
+          <motion.h1
+            variants={fadeIn("up", 0)}
+            initial="hidden"
+            whileInView={"show"}
+            className="motion-text-about"
+            viewport={{ once: false, amount: 0.7 }}
+          >
+            Создаём <br />
+            3D-motion
+          </motion.h1>
+          <motion.div
+            variants={fadeIn("up", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]"
+          >
+            <h1 className="motion-text"></h1>
+            <TypeAnimation
+              sequence={["Рекламу", 2000, "Дизайн", 2000]}
+              speed={50}
+              className="motion-text"
+              wrapper="h1"
+              repeat={Infinity}
+              href="#"
+            />
+          </motion.div>
+          
+          <span>
+            <p>для брендов</p>
+            <p>для приложении</p>
+            <p>сервисов</p>
+          </span>
+        </div>
+        <Logo />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
